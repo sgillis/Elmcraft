d9de2126afc1eb93a4ddb8473b1c1aede8c4720a
San Gillis <gillis.san@gmail.com>
2014-09-07 13:28:06 +0200

    Model world

--- src/Model.elm
+++ src/Model.elm
@@ -1,6 +1,7 @@
 module Model where
 
 import Math.Vector3 (..)
+import Math.Matrix4 (..)
 
 data Inputs = TimeDelta Float { x:Int, y:Int } Bool Bool
             | Mouse (Int, Int)
@@ -24,3 +25,17 @@ direction person =
     let h = person.horizontalAngle
         v = person.verticalAngle
     in vec3 (cos h) (sin v) (sin h)
+
+type Block =
+    { translation : Mat4 }
+
+type Blocks = [Block]
+
+initialWorld : Blocks
+initialWorld =
+    concat <|
+    map oneRow [-20..20]
+
+oneRow : Float -> Blocks
+oneRow z =
+    map (\x -> { translation = makeTranslate3  x -1 z }) [-20..20]
